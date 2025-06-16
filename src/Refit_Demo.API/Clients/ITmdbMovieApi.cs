b6565a2e290f370cac046500aab49fa5d4d73b1b
kauatwn <kaualopescontato@gmail.com>
2025-06-16 18:42:51 -0300

    Adiciona ITmdbMovieApi para integração com TMDB

---
+++ src/Refit_Demo.API/Clients/ITmdbMovieApi.cs
@@ -0,0 +1,17 @@
+﻿using Refit;
+using Refit_Demo.API.DTOs.Responses;
+
+namespace Refit_Demo.API.Clients;
+
+[Headers("accept: application/json")]
+public interface ITmdbMovieApi
+{
+    [Get("/movie/{movieId}")]
+    Task<MovieDetailsResponse> GetMovieDetailsAsync(int movieId, [Query] string language);
+
+    [Get("/trending/movie/{timeWindow}")]
+    Task<MovieListResponse> GetTrendingMoviesAsync(string timeWindow, [Query] string language);
+
+    [Get("/search/movie")]
+    Task<MovieListResponse> SearchMoviesAsync([Query] string query, [Query] string language, [Query] int page);
+}
