f6000710d10f27cbb85e3440b345f9edd8d9d596
kauatwn <kaualopescontato@gmail.com>
2025-06-16 18:47:47 -0300

    Configura serialização JSON e injeção de serviços da API

--- src/Refit_Demo.API/Program.cs
+++ src/Refit_Demo.API/Program.cs
@@ -1,11 +1,21 @@
+using Refit_Demo.API.Extensions;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTmdbApi(builder.Configuration);
+builder.Services.AddServices();
 
 var app = builder.Build();
 
