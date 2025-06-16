4f0dbcd1030fc2252792c01871fce2a48d1ea604
kauatwn <kaualopescontato@gmail.com>
2025-06-16 18:30:13 -0300

    Criando DTOs para tipar resposta da API TMDB

---
+++ src/Refit_Demo.API/DTOs/Responses/MovieDetailsResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Refit_Demo.API.DTOs.Responses;
+
+public sealed record MovieDetailsResponse(
+    int Id,
+    string? Title,
+    string? OriginalTitle,
+    string? Overview,
+    string? ReleaseDate,
+    List<MovieDetailsResponse.Genre> Genres)
+{
+    public sealed record Genre(int Id, string? Name);
+}
---
+++ src/Refit_Demo.API/DTOs/Responses/MovieListResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Refit_Demo.API.DTOs.Responses;
+
+public sealed record MovieListResponse(
+    int Page,
+    List<MovieListResponse.MovieSummary> Results,
+    int TotalPages,
+    int TotalResults)
+{
+    public sealed record MovieSummary(
+        int Id,
+        string? Title,
+        string? OriginalTitle,
+        string? Overview,
+        string? ReleaseDate);
+}
