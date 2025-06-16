c9cfd4f06a36cc28b2c1cf3118118dc939c85b0f
kauatwn <kaualopescontato@gmail.com>
2025-06-16 18:46:25 -0300

    Adiciona ITmdbService e implementação para chamadas à API de filmes

---
+++ src/Refit_Demo.API/Services/Interfaces/IMovieService.cs
@@ -0,0 +1,10 @@
+﻿using Refit_Demo.API.DTOs.Responses;
+
+namespace Refit_Demo.API.Services.Interfaces;
+
+public interface IMovieService
+{
+    Task<MovieDetailsResponse> GetMovieDetailsAsync(int movieId, string language);
+    Task<MovieListResponse> GetTrendingMoviesAsync(string timeWindow, string language);
+    Task<MovieListResponse> SearchMoviesAsync(string query, string language, int page);
+}
---
+++ src/Refit_Demo.API/Services/TmdbService.cs
@@ -0,0 +1,23 @@
+﻿using Refit_Demo.API.Clients;
+using Refit_Demo.API.DTOs.Responses;
+using Refit_Demo.API.Services.Interfaces;
+
+namespace Refit_Demo.API.Services;
+
+public class TmdbService(ITmdbMovieApi movieApi) : IMovieService
+{
+    public async Task<MovieDetailsResponse> GetMovieDetailsAsync(int movieId, string language)
+    {
+        return await movieApi.GetMovieDetailsAsync(movieId, language);
+    }
+
+    public async Task<MovieListResponse> GetTrendingMoviesAsync(string timeWindow, string language)
+    {
+        return await movieApi.GetTrendingMoviesAsync(timeWindow, language);
+    }
+
+    public async Task<MovieListResponse> SearchMoviesAsync(string query, string language, int pageNumber)
+    {
+        return await movieApi.SearchMoviesAsync(query, language, pageNumber);
+    }
+}
